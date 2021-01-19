d69f995d6524dbbe352a1b0737a1fc28ebc6f3c6
Serkan Çelik <serkancelik9090@hotmail.com>
2021-01-19 22:52:19 +0300

    Deneme Ödevi

---
+++ ODEV/CustomerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ODEV
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Ad + " " + customer.Soyad + " isimli " + customer.Id + " Id numaralı yeni kişi eklendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Ad + " " + customer.Soyad + " isimli " + customer.Id + " Id numaralı yeni kişi silindi");
+        }
+        public void List()
+        {
+            Customer customer1 = new Customer();
+            customer1.Ad = "Serkan";
+            customer1.Soyad = "Çelik";
+            customer1.Id = 1;
+
+            Customer customer2 = new Customer();
+            customer2.Ad = "Ayşe";
+            customer2.Soyad = "Çelik";
+            customer2.Id = 2;
+
+            Customer customer3 = new Customer();
+            customer3.Ad = "Samet";
+            customer3.Soyad = "Çelik";
+            customer3.Id = 3;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Ad + " " + customer.Soyad + " " + customer.Id);
+            }
+
+        }
+
+    }
+}
--- ODEV/Program.cs
+++ ODEV/Program.cs
@@ -6,7 +6,24 @@ namespace ODEV
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Ad = "Ahmet";
+            customer1.Soyad = "Öztürk";
+            customer1.Id = 4;
+
+            Customer customer2 = new Customer();
+            customer2.Ad = "Ali";
+            customer2.Soyad = "Demir";
+            customer2.Id = 5;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("MÜŞTERİLER :");
+            customerManager.List();
         }
     }
 }
